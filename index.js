bc9aaa07ee6068e2dc8ea6bd6e527f36ce5f4c83
David Björklund <david.bjorklund@gmail.com>
2014-11-16 14:56:12 +0100

    simplify

--- index.js
+++ index.js
@@ -7,32 +7,21 @@ function head (onHead, opts) {
   
   var rest = false
   var ended = false
-  var stream = through.obj(write, end)
+  var stream = through.obj(write)
   
   return stream
   
   function write(chunk, _, callback) {
     var self = this
     if (rest) return callback(null, chunk)
-    self.pause()
     onHead(chunk, function next(err) {
       if (err) {
-        self.resume()
-        // self.emit('error', err)
         callback(err)
-        self.push(null)
         return
       }
       if (opts.includeHead) self.push(chunk)
       rest = true
-      self.resume()
-      if (ended) self.push(null)
-        callback()
+      callback()
     })
-  }
-  
-  function end() {
-    ended = true
-    if (!this.paused) this.push(null)
   }
 }
